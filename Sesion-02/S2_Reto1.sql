41132b3035bd35f36d1ea54f616622cd7fe319c2
Briseida Esquivel <70351383+BriseidaGEB@users.noreply.github.com>
2020-10-29 18:21:00 -0600

    S2
    Retos sesión 2

---
+++ Sesion-02/S2_Reto1.sql
@@ -0,0 +1,16 @@
+#RETO 1
+
+#¿Qué artículos incluyen la palabra Pasta en su nombre?
+SELECT * FROM articulo  WHERE nombre LIKE '%Pasta%';
+
+#¿Qué artículos incluyen la palabra Cannelloni en su nombre?
+SELECT * FROM articulo  WHERE nombre = '%Canelloni%';
+
+#¿Qué nombres están separados por un guión (-) por ejemplo Puree - Kiwi?
+SELECT * FROM articulo  WHERE nombre LIKE '%-%';
+
+#¿Qué puestos incluyen la palabra Designer?
+SELECT * FROM puesto  WHERE nombre LIKE '%Designer%';
+
+#¿Qué puestos incluyen la palabra Developer?
+SELECT * FROM puesto  WHERE nombre LIKE '%Developer%';
